445d839759e75799cd1ede47a8a6b2305b714b30
CSanprieto <csanchezsanc@uoc.edu>
2025-03-05 21:06:34 +0100

    basic player movement

--- SamuraiKitchen/Assets/Scripts/Player.cs
+++ SamuraiKitchen/Assets/Scripts/Player.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
+using System.Collections;
 
-public class Player
+public class Player : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5f;
+    void Update(){
+
+        Vector2 inputVector = new Vector2(0,0);
+
+        if(Input.GetKey(KeyCode.W)){
+            inputVector.y = +1;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            inputVector.y = -1;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            inputVector.x = -1;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            inputVector.x = +1;
+        }
+        
+        // normalized
+        inputVector = inputVector.normalized;
+        // move direction
+        Vector3 moveDir =  new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+
+    }
+
     
 }
