e79740dd4d7c306b7e3595bac36edbed211c414f
CSanprieto <csanchezsanc@uoc.edu>
2025-04-01 21:01:25 +0200

    Adding rice prefab and it iteractions
    added rice prefab and interaction to put in on top of the counters

--- SamuraiKitchen/Assets/Scripts/ClearCounter.cs
+++ SamuraiKitchen/Assets/Scripts/ClearCounter.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 public class ClearCounter : MonoBehaviour
 {
 
+    [SerializeField] private Transform ricePrefab;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact(){
         Debug.Log("hey interacting with clear counter");
+        Transform ricePrefabTransform = Instantiate(ricePrefab, counterTopPoint);
+        ricePrefabTransform.localPosition = Vector3.zero;
     }
 }
